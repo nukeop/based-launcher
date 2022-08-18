862811a7c1534677760487dc48ce663afbbf7fdd
nukeop <12746779+nukeop@users.noreply.github.com>
2022-08-19 01:41:35 +0200

    Palette

--- src/components/Palette/Palette.tsx
+++ src/components/Palette/Palette.tsx
@@ -1,6 +1,11 @@
+import styles from "./Palette.module.scss";
 import { TextField } from "@mui/material";
 import React from "react";
 
 export const Palette: React.FC = () => {
-  return <TextField />;
+  return (
+    <div className={styles.palette}>
+      <TextField fullWidth inputProps={{ style: { fontSize: "2rem" } }} />
+    </div>
+  );
 };

6b72a25e3e5a3ec3e53a31d33fda4209107caf47
nukeop <12746779+nukeop@users.noreply.github.com>
2022-09-27 23:39:59 +0200

    Add a test for bangs

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,8 @@
 import matchers from "@testing-library/jest-dom/matchers";
 import { expect } from "vitest";
+import { vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
 
 expect.extend(matchers);
+const fetchMock = createFetchMock(vi);
+fetchMock.enableMocks();

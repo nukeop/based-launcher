d0be51cb814182bc0c37d9a6b2b6bc18261f1ca6
nukeop <12746779+nukeop@users.noreply.github.com>
2022-10-04 00:52:12 +0200

    Get desktop entries on linux using rust

--- electron/typings.d.ts
+++ electron/typings.d.ts
@@ -1,4 +1,11 @@
+type RustDesktopEntry = {
+  path: string;
+  name: string;
+  description: string;
+  icon?: string;
+};
+
 declare module "*index.node" {
-  export const getDesktopEntries: () => object;
+  export const getDesktopEntries: () => RustDesktopEntry[];
   export const getAppsMac: () => string;
 }
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,10 +1,12 @@
-use std::fs;
-
 mod mac;
 
+use std::fs;
+
 use freedesktop_desktop_entry::{default_paths, DesktopEntry, Iter};
-use mac::get_apps_json;
+use freedesktop_icons::lookup;
 use neon::prelude::*;
+
+use mac::get_apps_json;
 
 fn get_desktop_entries(mut cx: FunctionContext) -> JsResult<JsArray> {
     let entries = cx.empty_array();
@@ -15,15 +17,33 @@ fn get_desktop_entries(mut cx: FunctionContext) -> JsResult<JsArray> {
                 let locale = Some("en-US");
                 let obj = cx.empty_object();
 
+                let path_str = path.to_str().unwrap();
+                let path_js_str = cx.string(path_str);
+                obj.set(&mut cx, "path", path_js_str)?;
+
                 let name = cx.string(entry.name(locale).unwrap_or_default().to_string());
                 obj.set(&mut cx, "name", name)?;
 
                 let description = cx.string(entry.comment(locale).unwrap_or_default().to_string());
                 obj.set(&mut cx, "description", description)?;
 
-                let icon = cx.string(entry.icon().unwrap_or_default().to_string());
+                let icon_name = entry.icon().unwrap_or_default().to_string();
+                let icon = lookup(&icon_name)
+                    .with_size(48)
+                    .find()
+                    .unwrap_or_default()
+                    .into_os_string()
+                    .into_string()
+                    .unwrap();
 
-                obj.set(&mut cx, "icon", icon)?;
+                if (icon.len() > 0) {
+                    let icon_value = cx.string(icon);
+
+                    obj.set(&mut cx, "icon", icon_value)?;
+                }
+
+                let len = entries.len(&mut cx);
+                entries.set(&mut cx, len, obj)?;
             }
         }
     }
@@ -34,10 +54,6 @@ fn get_apps_mac(mut cx: FunctionContext) -> JsResult<JsString> {
     let entries = cx.empty_array();
     let process = get_apps_json();
     let json = String::from_utf8_lossy(&process.stdout);
-
-    // entries.p
-
-    // Ok(entries)
 
     Ok(cx.string(json))
 }

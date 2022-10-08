6698f823e8c8e70b56ada98668850b18885d81df
nukeop <12746779+nukeop@users.noreply.github.com>
2022-10-09 01:48:25 +0200

    Remove support for mac

--- native/src/services/desktop_apps/mac.rs
+++ native/src/services/desktop_apps/mac.rs
@@ -1,58 +1,10 @@
-use std::process::Command;
-
 use super::{DesktopApp, DesktopAppsProvider};
 
 pub struct MacDesktopApps {}
 
 impl DesktopAppsProvider for MacDesktopApps {
     fn get_desktop_entries() -> Vec<DesktopApp> {
-        let mut entries = Vec::new();
-
-        let output = Command::new("system_profiler")
-            .arg("-json")
-            .arg("SPApplicationsDataType")
-            .output()
-            .expect("failed to execute process");
-
-        let json = String::from_utf8_lossy(&output.stdout);
-        let v: serde_json::Value = serde_json::from_str(&json).unwrap();
-
-        match v.as_object().unwrap().get("SPApplicationsDataType") {
-            Some(apps) => {
-                for app in apps.as_array().unwrap() {
-                    let path = app
-                        .as_object()
-                        .unwrap()
-                        .get("path")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-
-                    let name = app
-                        .as_object()
-                        .unwrap()
-                        .get("_name")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-
-
-                    let plist_path = format!("{}/Contents/Info.plist", path);
-                    let plist = plist::Value::from_file(plist_path).unwrap();
-
-                    let icon = plist
-                        .as_dictionary()
-                        .and_then(|dict| dict.get("CFBundleIconFile"))
-                        .and_then(|icon| icon.as_string()).unwrap();
-
-                    entries.push(DesktopApp::new(path, name, None, Some(icon.to_string())));
-                }
-            }
-            None => {}
-        }
-
-        entries
+        // Mac desktop apps are not supported yet
+        vec![]
     }
 }
